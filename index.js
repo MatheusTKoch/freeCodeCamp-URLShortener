3f00221974a79e106ef4121a52e0ff62a44f581a
Matheus <matheustrilhakoch@hotmail.com>
2024-01-01 16:47:10 -0300

    Implementing model and database

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +20,27 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+const urlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  shorturl: {
+    type: Number,
+    required: true
+  }
+});
+
+const url = mongoose.model('url', urlSchema);
+
+app.route('/api/:shorturl')
+.get(function(req, res) {
+    
+}).post(
+  function(req, res) {
+    var shorturl = req.params.shorturl;
 });
 
 app.listen(port, function() {
