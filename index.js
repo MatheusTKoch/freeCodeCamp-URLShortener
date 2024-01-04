ed32eda245295f3fb2de4452dfc6f719671f1e47
Matheus <matheustrilhakoch@hotmail.com>
2024-01-04 20:09:26 -0300

    Fixed validation and get url

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const dns = require('node:dns');
 
 mongoose.connect(process.env.DATABASE_URL)
 
@@ -38,25 +39,44 @@ const urlSchema = new mongoose.Schema({
 
 const urlModel = mongoose.model('url', urlSchema);
 
-app.route('/api/:shorturl')
+app.route('/api/shorturl/:shorturl')
 .get(function(req, res) {
-    var shorturl = req.params.shorturl;
-    var urlQuery = urlModel.find({short_url: shorturl}).exec();
-    urlQuery.then(function(doc) {
-      res.redirect(doc[0].original_url);
-    });
-  }).post(
-  function(req, done) {
-    var maxShortUrl = urlModel.find({__v: 0}).sort({short_url: - 1}).limit(1).exec();
-    maxShortUrl.then(function(doc) {
-      var newShortUrl = doc[0].short_url + 1;
-      var newURL = new urlModel({
-        original_url: req.body.url,
-        short_url: newShortUrl
-      });
-      newURL.save();
-    });
-  }
+  var shorturl = req.params.shorturl;
+  var urlQuery = urlModel.find({short_url: shorturl}).exec();
+  urlQuery.then(function(doc) {
+    res.redirect(doc[0].original_url);
+  });
+});
+
+app.route('/api/:shorturl')
+.post(
+  function(req, res) {
+    let originalUrl = req.body.url;
+    let urlObject = new URL(originalUrl);
+
+    dns.lookup(urlObject.hostname, (err) => {
+      if (err) {
+        res.json({'error': 'invalid url'})
+      } else {
+        var maxShortUrl = urlModel.find({__v: 0}).sort({short_url: - 1}).limit(1).exec();
+
+        maxShortUrl.then(function(doc) {
+          var newShortUrl = doc[0].short_url + 1;
+          var newURL = new urlModel({
+            original_url: originalUrl,
+            short_url: newShortUrl
+          });
+
+          res.json({
+            original_url: req.body.url,
+            short_url: newShortUrl
+          });
+
+          newURL.save();
+        });
+      }
+    }
+  )}
 );
 
 app.listen(port, function() {
