a85fcfd1c12e35e7aa794bf3fa87c52b90fe37cb
Matheus <matheustrilhakoch@hotmail.com>
2024-01-05 19:48:16 -0300

    Adjusting static files

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/index.html');
